8f8d6744d37592453861f641bd0e8b85a66049d8
Ankur Badola <kh2063@kh2063-temp.local>
2018-07-05 12:52:47 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req,res,next)=>{
+    var now =  new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+     console.log(log)
+     fs.appendFile('server.log', log + '\n', (error)=>{
+      console.log(error);
+     });
+     next();
+})
+
+// app.use((req,res,next)=>{
+//     res.render('maintance.hbs')
+//     //next();
+// })
+
+
+app.use(express.static(__dirname + '/public' ));
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello World!</h1>');
+
+    // res.send({
+    //     name: 'Ankur',
+    //     likes: [
+    //         'bodybuilding',
+    //         'Biking'
+    //     ]
+    // })
+
+    res.render('home.hbs',{
+        name: 'Ankur',
+        pageTitle: 'Home Page',
+        welcomeMessage:'Welcome to my website'
+    })
+});
+
+app.get('/about', (req, res) => {
+    // res.send('<h1>Hello World!</h1>');
+
+    // res.send({
+    //     name: 'Ankur',
+        
+    // })
+
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    // res.send('<h1>Hello World!</h1>');
+
+    res.send({
+        errorMessage: 'BAD REQUEST',
+        
+    })
+});
+
+
+
+app.listen(3000,()=>{
+    console.log('Server is up on port: 3000')
+})
+
+
